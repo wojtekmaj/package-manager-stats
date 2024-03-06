6be2306236d025ba35184a8b77bc11ee043f6d34
Wojciech Maj <kontakt@wojtekmaj.pl>
2024-03-06 14:51:50 +0100

    Add alternative queries

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,18 @@ import semver from 'semver';
 
 const CACHE_DIR = '.cache';
 const GITHUB_API_URL = 'https://api.github.com';
+// Default - most starred repositories, period
 const QUERY = 'stars:>1';
+// Alt 1 - repositories with 1000+ stars, created in the last year
+// const oneYearAgo = new Date();
+// oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+// oneYearAgo.setHours(0, 0, 0, 0);
+// const QUERY = `stars:>1000 created:>${oneYearAgo.toISOString().slice(0, 10)}`;
+// Alt 2 - repositories with 500+ stars, created in the last 6 months
+// const sixMonthsAgo = new Date();
+// sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 6);
+// sixMonthsAgo.setHours(0, 0, 0, 0);
+// const QUERY = `stars:>500 created:>${sixMonthsAgo.toISOString().slice(0, 10)}`;
 // Each GitHub search results page has 30 items, we can fetch 1000 results with 34 requests
 const MAX_PAGES = 34;
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
